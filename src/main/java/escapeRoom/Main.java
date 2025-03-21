7b40d3f592d08ecb2e693de5fe81310ad448f089
isaac-diez <isaac.diez@proton.me>
2025-03-21 22:58:23 +0100

    Feat: Menu for NEW ROOM is added.

--- src/main/java/escapeRoom/Main.java
+++ src/main/java/escapeRoom/Main.java
@@ -1,5 +1,6 @@
 package escapeRoom;
 
+import escapeRoom.Menu.menuRooms;
 import escapeRoom.gameArea.*;
 
 import java.util.ArrayList;
@@ -8,39 +9,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        //Clues are created with Factory Pattern
-        GameElement clue1 = ClueFactory.createClue("Enigma");
-        GameElement clue2 = ClueFactory.createClue("Indication");
-        GameElement clue3 = ClueFactory.createClue("Indication");
+        menuRooms.mainMenuRooms();
 
-        List<Integer> clues_id = new ArrayList<>();
-
-        clues_id.add(clue1.getId());
-        clues_id.add(clue2.getId());
-        clues_id.add(clue3.getId());
-
-        //Props as well the same way
-        GameElement prop1 = PropFactory.createProp("Spade");
-        GameElement prop2 = PropFactory.createProp("Closet");
-        GameElement prop3 = PropFactory.createProp("Mountain");
-
-        List<Integer> props_id = new ArrayList<>();
-
-        props_id.add(prop1.getId());
-        props_id.add(prop2.getId());
-        props_id.add(prop3.getId());
-
-        //Rooms are created with Builder pattern
-        RoomDirector roomDirector = new RoomDirector();
-        RoomBuilder roomBuilder = new RoomBuilder();
-
-        roomDirector.buildRoomLoveAffair(roomBuilder,"LoveEscape",clues_id,props_id);
-        Room room1 = roomBuilder.getResult();
-
-        System.out.println(room1);
-
-        System.out.println(room1.getRoomValue());
-
-        System.out.println(prop3.getValue());
     }
 }
--- src/main/java/escapeRoom/gameArea/RoomDirector.java
+++ src/main/java/escapeRoom/gameArea/RoomDirector.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class RoomDirector {
 
-    public void buildRoomLoveAffair(RoomBuilder roomBuilder, String name, List<Integer> clues_id, List<Integer> props_id){
+    public void buildRoom(RoomBuilder roomBuilder, String name, Theme theme, Difficulty difficulty, List<Integer> clues_id, List<Integer> props_id){
         roomBuilder.setRoomName(name);
-        roomBuilder.setRoomTheme(Theme.LOVEAFFAIR);
-        roomBuilder.setRoomDifficulty(Difficulty.EASY);
+        roomBuilder.setRoomTheme(theme);
+        roomBuilder.setRoomDifficulty(difficulty);
         roomBuilder.setRoomClues(clues_id);
         roomBuilder.setRoomProps(props_id);
     }
