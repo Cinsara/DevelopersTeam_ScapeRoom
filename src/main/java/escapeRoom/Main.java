c7a634c8f584480ca89cc8764ef3d48033ea61fc
isaac-diez <isaac.diez@proton.me>
2025-03-23 22:03:50 +0100

    Feat: decoupled room creating menus. Created RoomPrinter to print last room created and all the rooms.

--- src/main/java/escapeRoom/Main.java
+++ src/main/java/escapeRoom/Main.java
@@ -1,6 +1,9 @@
 package escapeRoom;
 
+import escapeRoom.Menu.NewRoomMenu;
 import escapeRoom.Menu.menuRooms;
+import escapeRoom.gameArea.Inventory.RoomPrinter;
+import escapeRoom.gameArea.RoomCreation.RoomCreationService;
 
 public class Main {
     public static void main(String[] args) {
