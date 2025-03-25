53b96d3df42f5c3386bb7756735e0f0eb39f3785
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-03-25 17:12:22 +0100

    modified class captain

--- src/main/java/escapeRoom/PeopleArea_Classes/Captain.java
+++ src/main/java/escapeRoom/PeopleArea_Classes/Captain.java
@@ -3,25 +3,14 @@ package escapeRoom.PeopleArea_Classes;
 import java.time.LocalDate;
 
 public class Captain extends User{
-    private String ticket_id;
+    private final int ticket_id;
 
-    public Captain(int id, String name, String lastname, String email, String phoneNumber,
-                   LocalDate dob, LocalDate dateRegistration, boolean notificationStatus) {
-        super(id,name, lastname, email, phoneNumber, dob, notificationStatus);
+    public Captain(User user, int ticket_id) {
+        super(user.getId(), user.getName(), user.getLastname(), user.getEmail(), user.getPhoneNumber(), user.getDob(), user.isNotificationStatus());
         this.ticket_id = ticket_id;
     }
 
-    public Captain(String name, String lastname, String email, String phoneNumber,
-                   LocalDate dob, LocalDate dateRegistration, boolean notificationStatus) {
-        super(name, lastname, email, phoneNumber, dob, notificationStatus);
-        this.ticket_id = ticket_id;
-    }
-
-    public String getTicket_id() {
+    public int getTicket_id() {
         return ticket_id;
-    }
-
-    public void setTicket_id(String ticket_id) {
-        this.ticket_id = ticket_id;
     }
 }
