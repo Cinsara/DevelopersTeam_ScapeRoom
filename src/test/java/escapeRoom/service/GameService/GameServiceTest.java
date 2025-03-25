095f4962989c479b7c1e2c165f0988a298e8c181
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-03-25 13:39:23 +0100

    done GameService

--- src/test/java/escapeRoom/service/GameService/GameServiceTest.java
+++ src/test/java/escapeRoom/service/GameService/GameServiceTest.java
@@ -1,21 +1,25 @@
 package escapeRoom.service.GameService;
 
 import escapeRoom.gameArea.GameBuilder.Game;
-import escapeRoom.gameArea.GameBuilder.GameBuilder;
+import escapeRoom.gameArea.GameBuilder.GameMaker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameServiceTest {
+    static GameMaker gameMaker;
     static GameService service;
 
     @BeforeAll
     static void setUp() throws SQLException {
         service = new GameService();
+        gameMaker = new GameMaker();
     }
     @Test
     void getTableName() {
@@ -23,25 +27,42 @@ class GameServiceTest {
 
     @Test
     void mapResultSetToEntity() {
+
     }
 
     @Test
     void create() throws SQLException {
-        Game newGame = GameBuilder.createGame(1, LocalDate.now());
+        GameMaker.GameBuilder builder = gameMaker.createGameBuilder(1,LocalDate.now());
+        Game newGame = builder.build();
         Game returnedGame = service.create(newGame);
-        //assertEquals(9,returnedGame.get_id());
+        assertEquals(5,returnedGame.get_id());
         assertEquals(newGame.getDate(),returnedGame.getDate());
     }
 
     @Test
-    void read() {
+    void read() throws SQLException {
+        Optional<Game> newGame = service.read(1);
+        assertTrue(newGame.isPresent());
+        Game game = newGame.get();
+        assertEquals(1,game.get_id());
+        assertTrue(game.isSuccess());
+        assertEquals(2024, game.getDate().getYear());
     }
 
     @Test
-    void update() {
+    void update() throws SQLException {
+        GameMaker.GameBuilder newBuilder = gameMaker.createGameBuilder(4,LocalDate.now());
+        Game newGame = newBuilder.set_id(4).setSuccess(true).setCaptain_id(2).setEllapsedTimeInSeconds(4800).build();
+        service.update(newGame);
     }
 
     @Test
-    void delete() {
+    void delete() throws SQLException {
+        assertFalse(service.delete(2));
+    }
+    @Test
+    void getAll() throws SQLException {
+        List<Game> games = service.getAllEntities(service.getConnection());
+        games.forEach(game -> System.out.println(game.toString()));
     }
 }
