003ae0763fba89bffb5ce9e1df1e53d53cd4cdf4
Cinsara <cintia.csr18@gmail.com>
2025-03-27 09:56:58 +0100

    Upload UserService with Testing

--- src/test/java/escapeRoom/service/forTesting/UserService/UserServiceTest.java
+++ src/test/java/escapeRoom/service/forTesting/UserService/UserServiceTest.java
@@ -1,7 +1,7 @@
-package escapeRoom.service.forTesting.UserService;
+package escapeRoom.Service.forTesting.UserService;
 
-import escapeRoom.PeopleService.PeopleClasses_Testing.User;
-import escapeRoom.PeopleService.UserService;
+import escapeRoom.PeopleArea.User;
+import escapeRoom.Service.PeopleService.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -11,16 +11,24 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class UserServiceTest {
     static UserService userService;
+    static User testUser;
 
     @BeforeAll
     static void setUp() throws SQLException {
         userService = new UserService();
+
+        testUser = new User(1,
+                "Test",
+                "User",
+                "test@example.com",
+                "123456789",
+                LocalDate.of(2000, 1, 1),
+                false
+        );
     }
 
     @Test
@@ -29,9 +37,9 @@ public class UserServiceTest {
                 1,
                 "Ali",
                 "Vazquez",
-                LocalDate.of(1999,12,2),
                 "gmail.com",
                 "9866363663",
+                LocalDate.of(1999,12,2),
                 true);
 
         User createUser = userService.create(newUser);
@@ -44,9 +52,9 @@ public class UserServiceTest {
                 1,
                 "Ali",
                 "Vazquez",
-                LocalDate.of(1999,12,2),
                 "gmail.com",
                 "9866363663",
+                LocalDate.of(1999,12,2),
                 true);
 
         User createUser = userService.create(newUser);
@@ -57,30 +65,46 @@ public class UserServiceTest {
 
     @Test
     void update() throws SQLException {
-        User newUser = new User(1, "Sani", "Lorec", LocalDate.of(1999, 2, 3),
-                "sani@gmail.com", "656783981", false);
-        userService.update(newUser);
+        User createdUser = userService.create(testUser);
 
-        Optional<User> optional = userService.read(1);
+        User updateData = new User(createdUser.getId(), "Sani", "Lorec",
+                "sani@gmail.com","656890345",  LocalDate.of(1999, 2, 3), false);
+        userService.update(updateData);
+
+        Optional<User> optional = userService.read(createdUser.getId());
         Assertions.assertTrue(optional.isPresent());
 
         User userFromDb = optional.get();
-        Assertions.assertEquals(newUser.getId(), userFromDb.getId());
-        Assertions.assertEquals(newUser.getName(), userFromDb.getName());
-        Assertions.assertEquals(newUser.getLastname(), userFromDb.getLastname());
-        Assertions.assertEquals(newUser.getEmail(), userFromDb.getEmail());
-        Assertions.assertEquals(newUser.getPhoneNumber(), userFromDb.getPhoneNumber());
-        Assertions.assertEquals(newUser.getDob(), userFromDb.getDob());
-        Assertions.assertEquals(newUser.isNotificationStatus(), userFromDb.isNotificationStatus());
+        Assertions.assertEquals(updateData.getId(), userFromDb.getId());
+        Assertions.assertEquals(updateData.getName(), userFromDb.getName());
+        Assertions.assertEquals(updateData.getLastname(), userFromDb.getLastname());
+        Assertions.assertEquals(updateData.getEmail(), userFromDb.getEmail());
+        Assertions.assertEquals(updateData.getPhoneNumber(), userFromDb.getPhoneNumber());
+        Assertions.assertEquals(updateData.getDob(), userFromDb.getDob());
+        Assertions.assertEquals(updateData.isNotificationStatus(), userFromDb.isNotificationStatus());
     }
 
     @Test
     void delete() throws SQLException {
-        User newUser = new User(1, "Sani", "Lorec", LocalDate.of(1999, 2, 3),
-                "sani@gmail.com", "656783981", false);
+        User newUser = new User("Sani", "Lorec",
+                "sani@gmail.com","656890345",  LocalDate.of(1999, 2, 3), false);
+        User createdUser = userService.create(testUser);
+        int userId = createdUser.getId();
 
+        Assertions.assertTrue(userService.delete(userId));
 
-        assertTrue(userService.delete(1));
+        Optional<User> deletedUser = userService.read(userId);
+        assertFalse(deletedUser.isPresent());
 
+        try(ResultSet rs = userService.getAll(userService.getConnection())){
+            boolean userExists = false;
+            while(rs.next()){
+                if (rs.getInt("customer_id") == userId) {
+                    userExists = true;
+                    break;
+                }
+            }
+            assertFalse(userExists, "The user should not exist after the delete");
+        }
     }
 }
