0558c60a5f457a29c821379c117d1cff63017cb2
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-03-31 17:32:58 +0200

    working on game manager

--- src/test/java/escapeRoom/Manager/GameManagerTest.java
+++ src/test/java/escapeRoom/Manager/GameManagerTest.java
@@ -3,11 +3,13 @@ package escapeRoom.Manager;
 import escapeRoom.Service.AssetService.TicketService;
 import escapeRoom.Service.GameService.GameService;
 import escapeRoom.Service.InputService.InputService;
+import escapeRoom.Service.PeopleService.UserService;
 import escapeRoom.model.GameArea.GameBuilder.Game;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,16 +22,18 @@ class GameManagerTest {
         InputService is = new InputService(new Scanner(System.in));
         GameService gs = new GameService();
         TicketService ts = new TicketService();
+        UserService us = new UserService();
+        gameManager = new GameManager(is,gs,ts,us);
 
-        gameManager = new GameManager();
     }
 
     @Test
     void createGame() {
-
+        gameManager.createGame(LocalDate.now(),2);
     }
 
     @Test
     void bookGame() {
+        gameManager.bookGame(LocalDate.now().plusDays(1),3,1);
     }
 }
