0eef98df6bb1164dfadfdc1c7435e20b5ffc4614
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-03-31 18:21:07 +0200

    remove unnecessary exception type

--- src/main/java/escapeRoom/Manager/AbsentUserException.java
+++
@@ -1,7 +0,0 @@
-package escapeRoom.Manager;
-
-public class AbsentUserException extends Exception{
-    AbsentUserException(int id){
-        super("No user with id number " + id + " in database");
-    }
-}
