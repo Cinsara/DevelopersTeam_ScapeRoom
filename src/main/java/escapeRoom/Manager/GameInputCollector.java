6ba131a43b2b9941c42e4384883259ea2c710ca2
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-04-02 11:40:10 +0200

    Game Controller done

--- src/main/java/escapeRoom/Manager/GameInputCollector.java
+++ src/main/java/escapeRoom/Manager/GameInputCollector.java
@@ -9,28 +9,14 @@ import escapeRoom.model.PeopleArea.User;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class GameInputCollector {
     static InputService inputService = new InputService(new Scanner(System.in));
-
-    static public GameCoordinates getGameCoordinates() throws SQLException {
-        LocalDate date = inputService.readDate("Introduce the date of the game you are interested in","yyyy MM dd");
-        RoomService roomService = new RoomService();
-        List<Room> rooms = roomService.getAllEntities(ConnectionManager.getConnection());
-        StringBuilder listrooms = new StringBuilder();
-        for (Room room: rooms){
-            listrooms.append("Room Number: ").append(room.getId()).append(" - Room Name: ").append(room.getName()).append(" - Room Theme: ").append(room.getTheme().name()).append(" - Room Difficulty: ").append(room.getDifficulty()).append("\n");
-        }
-        int roomId = inputService.readInt("Introduce the number of the room you want to play in :\n"+listrooms);
-        GameCoordinates newCoordinates = new GameCoordinates(date,roomId);
-        while (rooms.stream().filter(room -> room.getId()==newCoordinates.getRoomId()).toList().isEmpty()){
-            newCoordinates.setRoomId(inputService.readInt("Let's do it again. Make sure to chose the number of a room that exists :\n"+listrooms));
-        }
-        return newCoordinates;
-    }
 
     static public LocalDate getGameDate() {
         return inputService.readDate("Introduce the date of the game you are interested in","yyyy MM dd");
@@ -38,25 +24,37 @@ public class GameInputCollector {
     static public int getGameRoomId() throws SQLException {
         RoomService roomService = new RoomService();
         List<Room> rooms = roomService.getAllEntities(ConnectionManager.getConnection());
-        StringBuilder listrooms = new StringBuilder();
+        StringBuilder listRooms = new StringBuilder();
         for (Room room: rooms){
-            listrooms.append("Room Number: ").append(room.getId()).append(" - Room Name: ").append(room.getName()).append(" - Room Theme: ").append(room.getTheme().name()).append(" - Room Difficulty: ").append(room.getDifficulty()).append("\n");
+            listRooms.append("Room Number: ").append(room.getId()).append(" - Room Name: ").append(room.getName()).append(" - Room Theme: ").append(room.getTheme().name()).append(" - Room Difficulty: ").append(room.getDifficulty()).append("\n");
         }
-        return inputService.readInt("Introduce the number of the room you want to play in :\n"+listrooms);
+        AtomicInteger roomId = new AtomicInteger(inputService.readInt("Introduce the number of the room you want to play in :\n"+listRooms));
+        while (rooms.stream().filter(room -> room.getId()==roomId.get()).toList().isEmpty()){
+            roomId.set(inputService.readInt("Let's do it again. Make sure to chose the number of a room that exists :\n"+listRooms));
+        }
+        return roomId.get();
     }
 
     static public int getTargetCostumer() throws SQLException {
         UserService userService = new UserService();
         List<User> users = userService.getAllEntities(ConnectionManager.getConnection());
-        StringBuilder listusers = new StringBuilder();
+        StringBuilder listUsers = new StringBuilder();
         for (User user: users){
-            listusers.append("User ID: ").append(user.getId()).append(" // ").append(user.getName()).append(" ").append(user.getLastname()).append("\n");
+            listUsers.append("User ID: ").append(user.getId()).append(" // ").append(user.getName()).append(" ").append(user.getLastname()).append("\n");
         }
-        AtomicInteger returnedValue = new AtomicInteger(inputService.readInt("Introduce the ID of the customer your are interested in :\n"+listusers));
+        AtomicInteger returnedValue = new AtomicInteger(inputService.readInt("Introduce the ID of the customer your are interested in :\n"+listUsers));
         while(users.stream().filter(user -> user.getId()== returnedValue.get()).toList().isEmpty()){
-            returnedValue.set(inputService.readInt("Let's do it again. Make sure to introduce the ID of an existing customer:\n"+listusers));
+            returnedValue.set(inputService.readInt("Let's do it again. Make sure to introduce the ID of an existing customer:\n"+listUsers));
         }
         return returnedValue.get();
     }
 
+    static public int chooseGamesClassification(String bookedOrAvailable){
+        String menuBasis = " ----\n Do you want to see \n ---- \n";
+        StringBuilder listOptions = new StringBuilder();
+        listOptions.append("1. All the ").append(bookedOrAvailable).append(" games\n").append("2. Only the ").append(bookedOrAvailable).append(" games on a given date\n").append("3. Only the ").append(bookedOrAvailable).append(" games in a given room.\n").append("4. Take me back!");
+        int input;
+        do {input = inputService.readInt(menuBasis + listOptions);} while(!new HashSet<Integer>(List.of(1, 2, 3, 4)).contains(input));
+        return input;
+    }
 }
